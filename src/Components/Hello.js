2b1b3ebbd9d4b3fbb0c0f73c9278df132733052f
Donna L Campbell <dlcsoup51@me.com>
2019-02-17 17:02:30 -0800

    Initialize application

---
+++ src/Components/Hello.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+ class Hello extends Component {
+    state = {}
+
+    render() {
+        return(
+            <div>Hello, I'm a React App!</div>
+        )
+    }
+}
+
+export default Hello;
